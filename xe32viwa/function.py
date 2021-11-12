24bd99ccdd2886ac9d71531a1b555fa0f48fd8fc
Katja Kossira <k.kossira@web.de>
2021-11-12 13:43:14 +0100

    comments: commiting update of my first package

--- xe32viwa/function.py
+++ xe32viwa/function.py
@@ -8,26 +8,31 @@ Created on Fri Nov 12 11:53:02 2021
 
 import numpy as np
 import pylab as plt
-from ipywidgets import interact, fixed
 from PIL import Image
 
 
-newSize = (64,64)
-imarray = np.random.rand(100,100,3) * 255
-image = Image.fromarray(imarray.astype('uint8'))
-
-def imshowResized(X, imgresize):
-   
-    resized = image.resize(imgresize, Image.ANTIALIAS)
-    return resized
-
-
-
-figure, (number1, number2) = plt.subplots(1,2)
-
-number1.imshow(image)
-number1.title.set_text("original image: ")
-number2.imshow(imshowResized(image, newSize))
-number2.title.set_text("resized image: ")
+def imshowResized(X, imgresize=None):
+	
+	
+	if imgresize != None and len(imgresize) == 2:	
+		resized = X.resize(imgresize, Image.ANTIALIAS)
+	else:
+		resized = X
+	
+	figure, (number1, number2) = plt.subplots(1,2)
+	number1.imshow(X)
+	number1.title.set_text("original image: ")
+	number2.imshow(resized)
+	number2.title.set_text("resized image: ")
+	
+	return resized
 
 
+if __name__ == "__main__":
+	newSize = (64,64)
+	
+	imarray = np.random.rand(100,100,3) * 255
+	image = Image.fromarray(imarray.astype('uint8'))
+	
+	imshowResized(image, newSize)
+	
